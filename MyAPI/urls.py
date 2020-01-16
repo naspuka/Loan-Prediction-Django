3ef6fb6983171ddb711ead9f2a707c3555b1d451
naspuka <folanas10@gmail.com>
2020-01-17 00:39:57 +0100

    make it better

--- MyAPI/urls.py
+++ MyAPI/urls.py
@@ -1,18 +1,14 @@
 from django.views.generic import RedirectView
 from django.urls import path, include
-from django.conf.urls import url
 from . import views
 from rest_framework import routers
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = routers.DefaultRouter()
 router.register('MyAPI', views.ApprovalsView)
 urlpatterns = [
     path('', views.cxcontact, name = 'cxform'),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
     # path('api/', include(router.urls)),
     # path('status/', views.approveReject),
 ]
 
 
-urlpatterns += staticfiles_urlpatterns()
